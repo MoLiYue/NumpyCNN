36927e1fa7818581e982d21583d4539283ab25c7
MoLiYue <45062702+MoLiYue@users.noreply.github.com>
2021-06-16 13:23:59 +0800

    temp

--- NumpyCNN.py
+++ NumpyCNN.py
@@ -1,5 +1,6 @@
 from skimage import io
 import skimage
 
-image = skimage.io.imread("")
+image = skimage.io.imread(fname = "D:\OneDrive\Git_proj\NumpyCNN\WechatIMG1994.jpeg", as_grey = True)
+skimage.io.imsave(fname = "D:\OneDrive\Git_proj\NumpyCNN\test.jpeg", arr = image)
 
